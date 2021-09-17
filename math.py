b449c564056a2174edb4384be956ebdfda82f82a
ankan <ankan_mitra@persistent.com>
2021-09-17 22:43:39 +0530

    Implemented subs() fn

--- math.py
+++ math.py
@@ -3,7 +3,7 @@ def add(x,y):
     return x+y
 # Subs implimentation    
 def subtract(x,y):
-    pass
+    return x-y                 # on bug456 branch
 # Multiply implimentation    
 def multiply(x,y):
     pass
