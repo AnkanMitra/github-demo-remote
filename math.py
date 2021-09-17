cde628bbe78d810f9e9215d92b3ded65171854db
AnkanMitra <90903545+AnkanMitra@users.noreply.github.com>
2021-09-17 23:07:21 +0530

    Implemented substract() from remote repository

--- math.py
+++ math.py
@@ -3,7 +3,7 @@ def add(x,y):
     return x+y
 # Subs implimentation    
 def subtract(x,y):
-    pass
+    return x-y      # from remote repository
 # Multiply implimentation    
 def multiply(x,y):
     pass
