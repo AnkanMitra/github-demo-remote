e77086ded3eff09039f311147134bc6d1188d80e
ankan <ankan_mitra@persistent.com>
2021-09-17 23:09:10 +0530

    Implemented multiply() from local repo

--- math.py
+++ math.py
@@ -6,7 +6,7 @@ def subtract(x,y):
     return x-y      # from remote repository
 # Multiply implimentation    
 def multiply(x,y):
-    pass
+    return x*y          #from local repo
 # Divide implimentation
 def divide(x,y):
     pass
