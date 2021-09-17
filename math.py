ab6ebe47ce96b84ea7b35230137a11e34791821e
AnkanMitra <90903545+AnkanMitra@users.noreply.github.com>
2021-09-17 17:43:31 +0530

    Initial commit on math.py

--- math.py
+++ math.py
@@ -1,16 +1,9 @@
-#add imple
-def add(x,y):
-    return x+y    
-#subs imple
-def subtract(x,y):
-    return x-y      #on master branch
-#multiply imple
-def multiply(x,y):
-    return x*y      #on Bug456 branch
-#multiply imple
-def divide(x,y):
-    if y==0:
-        return DIVIDE_BY_ZERO_ERROR
-    else:
-        return x/y      #On master branch
 
+def add(x,y):
+    pass    
+def subtract(x,y):
+    pass
+def multiply(x,y):
+    pass
+def divide(x,y):
+    pass
